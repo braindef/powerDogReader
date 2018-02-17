ce185d217effc40a89e04d055f9f7076b9ed19e6
Marc Landolt <mail@marclandolt.ch>
2018-02-17 23:09:55 +0100

    tempsave

--- updateRRD.py
+++ updateRRD.py
@@ -1,5 +1,10 @@
 import time
 
+from pyrrd.rrd import RRD, RRA, DS
+from pyrrd.graph import DEF, CDEF, VDEF
+from pyrrd.graph import LINE, AREA, GPRINT
+from pyrrd.graph import ColorAttributes, Graph
+
 from pyrrd.rrd import RRD
 myRRD = RRD('A1_B1_S14.rrd')
 
@@ -9,9 +14,32 @@ myRRD.update()
 
 myRRD.info()
 
-"""
+
+
+exampleNum = 4
+filename = 'A1_B1_S1%s.rrd' % exampleNum
+graphfile = 'A1_B1_S1%s-small.png' % exampleNum
+graphfileLg = 'A1_B1_S1%s.png' % exampleNum
+
+day = 24 * 60 * 60
+week = 7 * day
+month = day * 30
+quarter = month * 3
+half = 365 * day / 2
+year = 365 * day
+
+endTime = int(round(time.time()))
+delta = day
+startTime = endTime - delta
+step = 300
+maxSteps = int((endTime-startTime)/step)
+
+# Let's create and RRD file and dump some data in it
+
+
+
 # Let's set up the objects that will be added to the graph
-def1 = DEF(rrdfile=myRRD.filename, vname='myspeed', dsName=ds1.name)
+def1 = DEF(rrdfile=myRRD.filename, vname='myspeed', dsName="speed")
 #def2 = DEF(rrdfile=myRRD.filename, vname='mysilliness', dsName=ds2.name)
 #def3 = DEF(rrdfile=myRRD.filename, vname='myinsanity', dsName=ds3.name)
 #def4 = DEF(rrdfile=myRRD.filename, vname='mydementia', dsName=ds4.name)
@@ -47,5 +75,4 @@ g.height = 400
 g.write()
 
 
-"""
 
