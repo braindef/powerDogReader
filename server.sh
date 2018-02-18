5bef0b7f2c5f2c4e857f9116688c9a9e5da0775a
Marc Landolt <mail@marclandolt.ch>
2018-02-18 13:41:57 +0100

    server script

--- server.sh
+++ server.sh
@@ -1,1 +1,10 @@
-for i in $(seq 1 100); do python solarGraph.py --update && python solarGraph.py --render && rrdtool dump ../B2_A2_S2_temp.rrd && sleep 300 ;done
+while [ 1 ]
+do
+	python solarGraph.py --update
+	python solarGraph.py --render
+	#rrdtool dump ../B2_A2_S2_temp.rrd
+	date
+        sleep 300
+done
+
+
